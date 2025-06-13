e2098c69ca0adba4dcf59f3779d73617b385e4fb
Arthur D'Aquino <deaquinodev@gmail.com>
2025-06-13 01:24:49 -0300

    [Video Game Discovery App] - Create a generic data fetch hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,47 +1,10 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
 
-interface GenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setIsLoading(true);
-    apiClient
-      .get<GenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return {
-    error,
-    genres,
-    isLoading,
-  };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
-
