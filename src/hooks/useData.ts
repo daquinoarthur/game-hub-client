8db1d465b84719c7358c3a65ea21312e050f52c8
Arthur D'Aquino <deaquinodev@gmail.com>
2025-07-24 15:20:07 -0300

    refactor: Fetch Genres With React Query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface Response<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
@@ -22,7 +22,7 @@ const useData = <T>(
 
       setIsLoading(true);
       apiClient
-        .get<Response<T>>(endpoint, {
+        .get<FetchResponse<T>>(endpoint, {
           signal: controller.signal,
           ...requestConfig,
         })
